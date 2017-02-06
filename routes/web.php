67e1077841ecd8be98aa068ac9ab31b7e41610d2
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-02-06 10:48:35 +0100

    add auth middleware to my_favorites route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', 'PostsController@index');
 Route::post('favorite/{post}', 'PostsController@favoritePost');
 Route::post('unfavorite/{post}', 'PostsController@unFavoritePost');
 
-Route::get('my_favorites', 'UsersController@myFavorites');
+Route::get('my_favorites', 'UsersController@myFavorites')->middleware('auth');
